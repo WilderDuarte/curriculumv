a895013b8ef4554477d946e3cd9f8121a7703b23
WilderDuarte <waduarten@ufpso.edu.co>
2024-05-10 14:26:25 -0500

    arrya y metodos

--- js/functions.js
+++ js/functions.js
@@ -41,6 +41,9 @@ function load_page(){
         showConfirmButton: false,
         timer: 2000
     });
+
+    document.getElementById("text").innerText = "El array a calcular es: "+array_num;
+    document.getElementById("impresion").value = array_num;
 }
 
 function send_form(){
@@ -71,3 +74,82 @@ function send_form(){
     }
 }
 
+// ARRAYS
+var array_semana = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+var array_gral = [1,2,2.5,"abc","Soy Wilder"];
+var array_num = [1,2,3,4,5,6,7,8,9];
+
+var array_multi= [
+    {name:"Wilder",age:32,color:"Verde"},
+    {name:"Anyi",age:33,color:"Rosa"},
+    {name:"Celeste",age:3,color:"Rosa"},
+    {name:"Antonella",age:2,color:"Blanco"}
+];
+
+var json_ejm = {
+    name: "Wilder",
+    lastname: "Duarte",
+    phone: "3168667751",
+    email: "waduarten@ufpso.edu.co"
+};
+
+console.log(array_semana);
+console.log(array_gral);
+console.log(array_num);
+console.log(array_multi);
+console.log(json_ejm);
+
+function action(){
+    // alert(array_num);
+    // Swal.fire(""+array_num);
+    // console.log(array_num);
+    // console.log(array_num.length);
+    var result = 0;
+    for(let i=0;i<array_num.length;i++){
+        result = result+array_num[i]
+    }
+    document.getElementById("result").innerHTML = "<strong>El resultado es:"+result+"</strong>";
+    document.getElementById("result").style.color = "green";
+    // CON UN FOR
+    var total = 0;
+    for(var j=0;j<array_multi.length;j++){
+        total = array_multi[j].age + total;
+    }
+    alert(total);
+    // CON UN WHILE
+    var total_dos = 0;
+    var w = 0;
+    while(w<array_multi.length){
+        total_dos = array_multi[w].age + total_dos
+        w++;
+    }
+    alert(total_dos);
+}
+
+var array_numerico = [1,2,3,4,5,6,7,8,9];
+
+function eliminar(){
+    let array_delete = array_numerico.pop();
+    console.log(array_delete);
+    console.log(array_numerico);
+    document.getElementById("impresion").value = array_numerico;
+}
+
+function agregar(){
+    let num = document.getElementById("num").value;
+    let array_add = array_numerico.push(num);
+    console.log(array_add);
+    console.log(array_numerico);
+    document.getElementById("impresion").value = array_numerico;
+}
+
+function reves(){
+    document.getElementById("impresion").value = array_numerico.reverse();
+}
+
+function limpiar(){
+    document.getElementById("num").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("last_name").value = "";
+    document.getElementById("result").innerText = "";
+}
